a508e4da30d80bc0ddaf32466d5b662d6594f5a8
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-11-12 21:36:45 -0300

    first commit

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Login() {
+  return (
+    <>
+      <div>Login</div>
+      <Link to={"/"} >Vá para a home</Link>
+    </>
+  )
+}
