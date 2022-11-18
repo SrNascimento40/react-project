1d31149346e4e6dd91fa4611d415fcff4cdcca52
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-11-18 15:17:46 -0300

    feature: add react hooks

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit 19528d2955209274e17a7ed309d1eda83a97304e[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Wallace Nascimento <wallacediogo10@hotmail.com>
+Date:   Tue Nov 15 19:52:15 2022 -0300
+
+    feature: style basic finish
+
+[33mcommit b30f1d1bb2b9ccac0e2b26a70d6febe030e6574c[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Wallace Nascimento <wallacediogo10@hotmail.com>
+Date:   Mon Nov 14 23:55:36 2022 -0300
+
+    feature: login page
+
+[33mcommit a508e4da30d80bc0ddaf32466d5b662d6594f5a8[m
+Author: Wallace Nascimento <wallacediogo10@hotmail.com>
+Date:   Sat Nov 12 21:36:45 2022 -0300
+
+    first commit
+
+[33mcommit c79b5c83cca6fa4bf4a6e8173e2193a92c911277[m
+Author: Wallace Nascimento <wallacediogo10@hotmail.com>
+Date:   Sat Nov 12 21:07:47 2022 -0300
+
+    Initialize project using Create React App
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import { MdEmail, MdLock } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
@@ -7,7 +10,22 @@ import Header from '../../components/Header'
 import Input from '../../components/Input/index'
 import { Column, Container, CriarText, EsqueciText, Row, SubTitleLogin, Title, TitleLogin, Wrapper } from './styles'
 
+
+const schema = yup.object({
+  email: yup.string().email('E-mail inválido').required("Campo obrigatório"),
+  password: yup.string().min(3, 'no mínimo 3 caracteres').required("Campo obrigatório"),
+}).required();
+
 export default function Login() {
+
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+    resolver:yupResolver(schema),
+    //valida a cada caractere digitado
+    mode: 'onChange'
+  });
+  console.log(isValid, errors);
+
+  const onSubmit = data => console.log(data);
 
   const navigate = useNavigate();
   const handleClickSignIn = () => {
@@ -27,10 +45,10 @@ export default function Login() {
           <Wrapper>
             <TitleLogin>Faça seu cadastro</TitleLogin>
             <SubTitleLogin>faça seu login e make the change._</SubTitleLogin>
-            <form>
-              <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
-              <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-              <Button title="entrar" variant="secondary" onClick={() => handleClickSignIn() } type='button' />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input name="email" control={control} placeholder="E-mail" errorMessage={errors?.email?.message} leftIcon={<MdEmail />}/>
+              <Input name="password" control={control} placeholder="Senha" errorMessage={errors?.password?.message} type="password" leftIcon={<MdLock />}/>
+              <Button title="entrar" variant="secondary" /* onClick={() => handleClickSignIn() } */ type='submit' />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
